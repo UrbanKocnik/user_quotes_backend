1fc2966995af0a96d0ce0f72a04441ea5624b850
Urban <urban.kocnik@gmail.com>
2022-10-21 01:06:57 +0200

    update user info and password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,39 +8,47 @@ import { UsersService } from './users.service';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
-@Controller('users')
+@Controller('me')
 export class UsersController {
     
     constructor(private userService: UsersService,
         private authService: AuthService){}
 
     @Get()
-    async all(){
-        return await this.userService.all();
+    async me(@Req() request: Request){
+        const id = await this.authService.userId(request);
+        return this.userService.findOneRelations({id})
     }
-/*
-    @Post()
-    @HasPermissions('users')
-    async create(@Body() body: UserCreateDto): Promise<User> {
-        const hash = await bcrypt.hash("1234", 12);
-        
-
-        return this.userService.create({
-            first_name: body.first_name,
-            last_name: body.last_name,
-            email: body.email,
-            password: hash,
-            role: {id: body.role_id} //to define a foreign key it has to be an object
-        });
-    }*/
 
     @Get(':id')
     async get(@Param('id') id:number){
         return this.userService.findOneRelations({id})
     }
 
-    @Delete(':id')
-    async delete(@Param('id') id:number){
-        return this.userService.delete(id);
+    @Put('update-info')
+    async updateInfo(
+        @Body() body: UserUpdateDto,
+        @Req() request: Request){
+
+        const id = await this.authService.userId(request);
+        await this.userService.update(id, body)
+        return this.userService.findOneRelations({id})
+    }
+
+    @Put('update-password')
+    async updatePassword(
+        @Body('password') password: string,
+        @Body('password_confirm') password_confirm: string,
+        @Req() request: Request){
+        if(password !== password_confirm){
+            throw new BadRequestException('Passwords do not match');
+        }
+        const id = await this.authService.userId(request);
+        const hash = await bcrypt.hash(password, 12);
+                   
+        await this.userService.update(id, {
+            password: hash
+        })
+        return this.userService.findOneRelations({id})
     }
 }
